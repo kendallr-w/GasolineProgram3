f7467bb898e60fd122817b8e347fe6c6a8e0576a
kendallr-w <reedwarrenkj@gmail.com>
2025-09-04 18:24:37 -0500

    Update GasolineProgram3.cpp
    assignment complete 6:24 pm 9/4/25

--- GasolineProgram3/GasolineProgram3.cpp
+++ GasolineProgram3/GasolineProgram3.cpp
@@ -2,24 +2,34 @@
 
 int main()
 {
-    std::cout << "Enter your car's fuel efficiency (in mpg (or kpl))\n";
+    double costPerGallon = 3.10;
+    
+    double tankCapacity;
+    
+    std::cout << "Please enter your car's fuel efficiency (in mpg (or kpl))\n";
 
-    int fuelEfficiency = 0;//= 57.7; //.7 gets "truncated"
+    double fuelEfficiency = 0;//= 57.7; //.7 gets "truncated"
     //intializing ALL variables when you declare them is a good idea
-    int tankCapacity;
+    
+    
 
     //remember what a "BREAKPOINT" is, it's useful
 
     std::cin >> fuelEfficiency;
-
-    std::cout << "You entered fuel efficiency = " << fuelEfficiency << " miles per gallon\n";
     
-    std::cout << "Enter your vehicle's fuel tank capacity = " << tankCapacity << " gallons\n";
+    
+    std::cout << "Enter your vehicle's fuel tank capacity (in gallons):\n";
     std::cin >> tankCapacity;
+    
+    double totalCost = tankCapacity * costPerGallon;
 
-    int tankCapacity = 13;// gallons"
-    int travelRange = fuelEfficiency * tankCapacity;
-
-    std::cout << "You can go this many miles on a full tank:\n"
+    
+    double travelRange = fuelEfficiency * tankCapacity;
+    
+    ////
+    
+    std::cout << "The current cost for gas is: $" << costPerGallon << '\n';
+    std::cout << "Therefore, you can go this many miles on a full tank:\n"
         << travelRange << "\n";
+    std::cout << "Total cost to fill the tank: $" << totalCost;
 }
