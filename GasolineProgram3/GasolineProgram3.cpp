de3d919bd18eb65cbd92344e814c195c00af530c
kendallr-w <reedwarrenkj@gmail.com>
2025-09-04 13:06:31 -0500

    Update GasolineProgram3.cpp
    updated 1:06

--- GasolineProgram3/GasolineProgram3.cpp
+++ GasolineProgram3/GasolineProgram3.cpp
@@ -6,12 +6,16 @@ int main()
 
     int fuelEfficiency = 0;//= 57.7; //.7 gets "truncated"
     //intializing ALL variables when you declare them is a good idea
+    int tankCapacity;
 
     //remember what a "BREAKPOINT" is, it's useful
 
     std::cin >> fuelEfficiency;
 
     std::cout << "You entered fuel efficiency = " << fuelEfficiency << " miles per gallon\n";
+    
+    std::cout << "Enter your vehicle's fuel tank capacity = " << tankCapacity << " gallons\n";
+    std::cin >> tankCapacity;
 
     int tankCapacity = 13;// gallons"
     int travelRange = fuelEfficiency * tankCapacity;
@@ -19,4 +23,3 @@ int main()
     std::cout << "You can go this many miles on a full tank:\n"
         << travelRange << "\n";
 }
-
